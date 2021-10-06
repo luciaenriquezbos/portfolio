c195c7d42d4ac7b33a5bfe77a53b9744cf04c4ac
luciaenriquezbos <eboscreative@gmail.com>
2021-10-06 23:02:38 +0200

    add library

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
-import "../stylesheets/App.css";
+import { makeStyles } from "@material-ui/core";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <h1>Este es mi portfolio</h1>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
 export default App;
