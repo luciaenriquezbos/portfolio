0c39fa39bbcf76627dca1f74949c31ae309a5232
luciaenriquezbos <eboscreative@gmail.com>
2021-11-17 14:09:09 +0100

    works

--- src/components/mockData.js
+++ src/components/mockData.js
@@ -1,5 +1,7 @@
 import React from "react";
 import listofcountries from "../Images/countries.jpg";
+import rickandmorty from "../Images/rick.jpg";
+import showlist from "../Images/dexter.jpg";
 
 const mockData = [
   {
@@ -10,11 +12,18 @@ const mockData = [
     link: "https://github.com/luciaenriquezbos/list-of-countries",
   },
   {
-    title: "ASK FOR A COUNTRY",
+    title: "RICK AND MORTY",
     description:
-      "Buscador de paises del mundo. Desarrollo aplicación responsive en JS. Permite buscar por nombre y continente. Listado de favoritos",
-    image: listofcountries,
-    link: "https://github.com/luciaenriquezbos/list-of-countries",
+      "Proyecto realizado con REACT. Página web que nos muestra: 1.Listado de personajes y un campo para filtrar por nombre. 2.Cada uno de los personajes nos muestra la información detallada en una nueva página",
+    image: rickandmorty,
+    link: "https://github.com/luciaenriquezbos/Listado-de-Personajes-de-RICK-MORTY",
+  },
+  {
+    title: "SHOWLIST",
+    description:
+      "Proyecto realizado en JS. Búsqueda con un fetch de tu serie favorita, con un listado de favoritos",
+    image: showlist,
+    link: "https://github.com/luciaenriquezbos/Listado-de-Personajes-de-RICK-MORTY",
   },
 ];
 
